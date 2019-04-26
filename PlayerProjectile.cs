c0611735d17b9f233d73cf2e68ad2be11f1128e5
Dylan Tuck <45218496+Keli2k@users.noreply.github.com>
2019-04-25 21:35:40 -0400

    Add files via upload

---
+++ PlayerProjectile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+    float travelTime = 1;
+    public Animator animator;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        travelTime -= Time.deltaTime;
+        if (travelTime <= 0)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ PlayerRangedAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRangedAttack : MonoBehaviour
+{
+    private float attackCooldown;
+    public float startAttackCooldown;
+    public LayerMask findEnemy;
+    public int damage;
+    public Animator animator;
+    public GameObject projectile;
+
+
+    void Update()
+    {
+        if (attackCooldown <= 0)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                animator.SetTrigger("Shoot");
+            }
+        }
+    }
+}
